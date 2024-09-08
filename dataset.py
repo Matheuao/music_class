64eddce4112fa7ba64606398c0c9952f6376c660
Matheuao <110135464+Matheuao@users.noreply.github.com>
2024-09-08 19:37:45 -0300

    Add files via upload
    adding a dataset manipulation script for generate the features of the model

---
+++ dataset.py
@@ -0,0 +1,81 @@
+from utils import *
+import os
+
+
+
+def dataset_augmentation(data_origen_path,data_transformation_path):
+
+
+    rechannel_path = data_transformation_path + "/rechannel"
+    os.mkdir(rechannel_path)
+
+    augment_path = data_transformation_path + "/data_augmented"
+    os.mkdir(augment_path)
+
+    for folder in os.listdir(data_origen_path):
+        os.mkdir(rechannel_path +"/" + folder)
+        os.mkdir(augment_path +"/" + folder)
+
+        for file in os.listdir(data_origen_path + "/" + folder):
+
+            audio_tensor, sr = load_audio(data_origen_path + "/" + folder + "/" + file)
+            audio_rechannel = rechannel(audio_tensor) #todas estão em estério e todas tem o mesmo sample rate    
+            save_audio(path= rechannel_path + "/" + folder + "/" + file, aud=audio_rechannel,  sr=sr)
+
+            audio_time_shift = time_shift(audio_rechannel, sr, 3333)
+            audio_pitch_shift = pitch_shift(audio_time_shift[0].numpy(), sr)
+            save_audio(path= augment_path + "/" + folder + "/" + file, aud=torch.from_numpy(audio_pitch_shift),  sr=sr)
+
+def dataset_features(path, augment = "yes"):
+
+    path_spec_original = path + "/" + "spec_original"
+    path_spec_augmented = path + "/" + "spec_augmented"
+    path_spec_mask_augmented = path + "/" + "spec_mask_augmented"
+    
+    os.mkdir(path_spec_original)
+    os.mkdir(path_spec_augmented)
+    os.mkdir(path_spec_mask_augmented)
+
+    if augment == "yes":
+        rechannel_path = path +"/rechannel"
+        data_augmented_path = path + "/data_augmented"
+
+        for dir in os.listdir(rechannel_path ):
+            path_new_dir = path_spec_original + "/" + dir;
+            os.mkdir(path_new_dir)
+            create_pngs_from_wavs(rechannel_path + "/" + dir,path_new_dir)
+
+        for dir in os.listdir(data_augmented_path):
+            path_new_dir = path_spec_augmented + "/" + dir;
+            os.mkdir(path_new_dir)
+            create_pngs_from_wavs(data_augmented_path + "/" + dir,path_new_dir)
+
+        augmented_spec_loop(data_augmented_path, path + "/spec_mask_augmented")
+
+    elif augment == "no" :
+        rechannel_path = path +"/rechannel"
+        for dir in os.listdir(rechannel_path ):
+            path_new_dir = path_spec_original + "/" + dir;
+            os.mkdir(path_new_dir)
+            create_pngs_from_wavs(rechannel_path + "/" + dir,path_new_dir)
+    
+    else: print("expext a 'yes' or 'no' enswer")
+
+
+
+def dataset_creation(origen_path,transform_path, augment = "yes"):
+    #origen_path: path to the dataset,
+    #transform_path : path to the augmentation and spectrograms
+    if augment == "yes":
+        dataset_augmentation(origen_path,transform_path)
+        dataset_features(transform_path)
+
+    elif augment == "no":
+        print("todo")
+
+    else :print("expext a 'yes' or 'no' enswer")
+
+
+dataset_creation("dataset/BallroomData","dataset")
+
+
---
+++ utils.py
@@ -0,0 +1,161 @@
+import math, random
+import torch
+import torchaudio
+from torchaudio import transforms
+import librosa
+import librosa.display
+import soundfile as sf
+import matplotlib.pyplot as plt
+import os
+
+# AUDIO FILE MANIPULATION
+def load_audio(file):
+    audio_tensor, sr = torchaudio.load(file)
+    
+    return audio_tensor, sr
+
+def save_audio(aud, path, sr):
+    #sf.write(path, aud, samplerate=sr, 'PCM_24')
+
+    torchaudio.save(path, aud, sample_rate=sr)
+    
+def rechannel(aud):
+    
+    # Convert from mono to stereo by duplicating the first channel
+    resig = torch.cat([aud, aud])
+
+    return ((resig))
+
+def resample(aud, sr, newsr):
+    sig = aud
+
+    if (sr == newsr):
+      # Nothing to do
+      return aud
+
+    num_channels = sig.shape[0]
+    # Resample first channel
+    resig = torchaudio.transforms.Resample(sr, newsr)(sig[:1,:])
+    if (num_channels > 1):
+        # Resample the second channel and merge both channels
+        retwo = torchaudio.transforms.Resample(sr, newsr)(sig[1:,:])
+        resig = torch.cat([resig, retwo])
+
+    return ((resig, newsr))
+
+def mp32wav(src, dst):
+    subprocess.call(['ffmpeg', '-i', src, dst])
+    
+def crop_audio(aud, sr, time_frame):
+    walk = int(random.uniform(0,aud.shape[1]))
+    aud = aud[:,walk:walk+time_frame*sr]
+              
+    return aud
+
+# AUDIO AUGMENTATION
+def time_shift(aud, sr, shift_limit):
+    audio_len = aud.shape
+    shift_amt = round(random.random() * shift_limit * audio_len[1])
+    aud_shifted = aud.roll(shift_amt), sr
+    
+    return aud_shifted
+
+def pitch_shift(aud, sr):
+    shift_amt = int(random.uniform(-1,1)*5+1)
+    waveform = librosa.effects.pitch_shift(y=aud,sr=sr,n_steps=shift_amt)
+    
+    return waveform
+
+# AUDIO SPECTROGRAM MANIPULATION AND AUGMENTATION
+def create_spectrogram(audio_file, image_file):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
+    y, sr = torchaudio.load(audio_file)
+    ms = transforms.MelSpectrogram(sr, n_fft=1024, hop_length=None, n_mels=64)(y)
+    log_ms = transforms.AmplitudeToDB(top_db=80)(ms)
+    librosa.display.specshow(torch.Tensor.numpy(log_ms[0]),sr=sr)
+
+    fig.savefig(image_file)
+    plt.close(fig)
+    
+    return log_ms
+    
+def create_pngs_from_wavs(input_path, output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    dir = os.listdir(input_path)
+
+    for i, file in enumerate(dir):
+        input_file = os.path.join(input_path, file)
+        output_file = os.path.join(output_path, file.replace('.wav', '.png'))
+        create_spectrogram(input_file, output_file)
+        
+# SPECTROGRAM AUGMENTATION
+def spectro_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
+    _, n_mels, n_steps = spec.shape
+    mask_value = spec.mean()
+    aug_spec = spec
+
+    freq_mask_param = max_mask_pct * n_mels
+    for _ in range(n_freq_masks):
+        aug_spec = transforms.FrequencyMasking(freq_mask_param)(aug_spec, mask_value)
+
+    time_mask_param = max_mask_pct * n_steps
+    for _ in range(n_time_masks):
+        aug_spec = transforms.TimeMasking(time_mask_param)(aug_spec, mask_value)
+
+    return aug_spec
+
+# FUNÇOES PARA GERAR ARRAY DE IMAGEM
+# Pop - 0
+# Metal - 1
+# Disco - 2
+# Blues - 3
+# Reggae - 4 
+# Classical - 5  
+# Rock - 6 
+# Hip-Hop - 7
+# Country - 8
+# Jazz - 9
+
+def augmented_spec_loop(path_in, path_out):
+   
+
+    for folder in os.listdir(path_in):
+
+        os.mkdir( path_out + "/" + folder)
+
+        for file in os.listdir(path_in + "/" + folder):
+            fig = plt.figure()
+            ax = fig.add_subplot(1, 1, 1)
+            fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+            #../dataset/genres_augmented"+ "/" + folder + "/" + file
+            audio_tensor, sr = load_audio(path_in + "/" + folder + "/" + file)
+            ms = transforms.MelSpectrogram(sr, n_fft=1024, hop_length=None, n_mels=64)(audio_tensor)
+            log_ms = transforms.AmplitudeToDB(top_db=80)(ms)
+            spec_aug = spectro_augment(log_ms)
+            librosa.display.specshow(torch.Tensor.numpy(spec_aug[0]),sr=sr)
+            
+            png = file.replace('.wav', '.png')
+
+            fig.savefig(path_out + "/" + folder + "/" + png)
+            plt.close(fig)
+
+def load_images_from_path(path, label):
+    images = []
+    labels = []
+
+    for file in os.listdir(path):
+        images.append(image.img_to_array(image.load_img(os.path.join(path, file), target_size=(int(480/1.5), int(640/1.5), 3))))
+        labels.append((label))
+        
+    return images, labels
+
+def show_images(images):
+    fig, axes = plt.subplots(1, 8, figsize=(20, 20), subplot_kw={'xticks': [], 'yticks': []})
+
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i] / 255)
